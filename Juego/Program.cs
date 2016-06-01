8fe2b0acb754426b0fbdf56b4307f873c832602d
María Fernanda <mfgs97@hotmail.com>
2016-05-31 20:56:39 -0600

    Tarea3

---
+++ Juego/Program.cs
@@ -0,0 +1,452 @@
+﻿using System;
+
+
+namespace Juego
+{
+    class Program
+    {
+        static int cont = 0;
+        static int cont2 = 0;
+        static int option = 0;
+        static void BinaryConversion()
+        {
+
+
+            int num = 0;
+
+
+            Console.Clear();
+
+            Console.WriteLine("1.¿Cuanto es 10001 de binario a octal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 21)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+
+            Console.WriteLine("2.¿Cuanto es 26 de octal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 10110)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("3.¿Cuanto es 77 de octal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 111111)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("4.¿Cuanto es 1111111 de binario a octal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 177)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("5.¿Cuanto es 111 de octal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 1001001)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("6.¿Cuanto es 10101010 de binario a octal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 252)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("7.¿Cuanto es 000111 de binario a octal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 7)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("8.¿Cuanto es 210 de octal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 10001000)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("9.¿Cuanto es 56 de octal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 101110)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("10.¿Cuanto es 10101 de binario a octal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 25)
+            {
+                cont++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+        }
+
+        static void hexa()
+        {
+
+
+            int num = 0;
+
+            Console.Clear();
+            Console.WriteLine("1.¿Cuanto es 11001 de binario a hexadecimal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 19)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+
+            Console.WriteLine("2.¿Cuanto es 146 de hexadecimal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 101000110)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("3.¿Cuanto es 345 de hexadecimal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 1101000101)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("4.¿Cuanto es 1010101 de binario a hexadecimal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 55)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("5.¿Cuanto es 11 de binario a hexadecimal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 3)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("6.¿Cuanto es 6 de hexadecimal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 110)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("7.¿Cuanto es 33 de hexadecimal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 110011)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("8.¿Cuanto es 1000 de binario a hexadecimal?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 8)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("9.¿Cuanto es 111000 de binario a hexadecimal ? ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 38)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+            Console.Clear();
+            Console.WriteLine("10.¿Cuanto es 99 de hexadecimal a binario?");
+            num = int.Parse(Console.ReadLine());
+            if (num == 10011001)
+            {
+                cont2++;
+                Console.WriteLine("Ganaste un punto");
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+                Console.ReadKey();
+
+            }
+
+
+
+
+        }
+
+        static void resultados()
+        {
+            int total = 0;
+            total = cont + cont2;
+            Console.Clear();
+            Console.WriteLine("Puntos primer juego: " + cont);
+            Console.WriteLine("Puntos segundo juego: " + cont2);
+            Console.WriteLine();
+            if (total > 8)
+            {
+                Console.WriteLine("Ganastes");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine("Perdiste");
+                Console.ReadKey();
+
+            }
+
+
+        }
+
+
+
+
+        static void Main()
+        {
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--Juego de agilidad numérica--");
+                Console.WriteLine("Seleccione una Clasificación.");
+                Console.WriteLine("1. Binario-Octal o Octal-Binario");
+                Console.WriteLine("2. Binario-Hexadecimal o Hexadecimal-Binario");
+                Console.WriteLine("3. Ver notas");
+                Console.WriteLine("4. No Jugar");
+
+
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+
+                            BinaryConversion();
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            hexa();
+                            break;
+                        }
+                    case 3:
+                        {
+                            resultados();
+                            break;
+                        }
+                    case 4:
+                        {
+
+                            break;
+                        }
+
+
+                }
+            } while (option != 4);
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Lista Estudiantes/Program.cs
@@ -0,0 +1,400 @@
+﻿using System;
+
+
+namespace ListaEstudiantes
+{
+    class Program
+    {
+
+        static int option = 0;
+        static int i = 0;
+        static int size = 5;
+        static int size2;
+        static int cont1 = 0;
+        static int cont2 = 0;
+        static string fem = "F";
+        static string mas = "M";
+        static double minors = 0;
+        static int young = 0;
+        static int adults = 0;
+
+
+
+
+
+        struct list
+        {
+            public int studentcode;
+            public string studentname;
+            public string studentsex;
+            public double age;
+
+        };
+
+
+
+
+        static list[] lists;
+
+        static void count()
+        {
+            Console.Clear();
+
+
+            Console.WriteLine("======================================================================================\n" +
+
+                                                "                  " + "Informe Clasificación estudiantil UH\n" +
+                           "=====================================================================================");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Universidad UH");
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Clasificación por edad    ||   Clasificación por Género ");
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine("Menores de edad: " + minors + "        " + "||" + "     " + "Hombres:   " + cont2);
+            Console.WriteLine("Jóvenes: " + "        " + young + "        " + "||" + "     " + "Mujeres:   " + cont1);
+            Console.WriteLine("Adultos: " + "        " + adults + "        " + "||");
+            Console.WriteLine(" ");
+            Console.WriteLine("=====================================================================================");
+            Console.WriteLine("Fin del informe");
+            Console.WriteLine();
+            Console.WriteLine("Presione la tecla INTRO para volver al menú");
+
+
+            Console.ReadKey();
+        }
+
+        static void AddStudents()
+        {
+
+            int r = 0;
+
+            while (r != 1)
+            {
+                Console.Clear();
+                if (i < lists.Length)
+                {
+                    Console.WriteLine("Universidad UH");
+                    Console.WriteLine("Ingreso de Estudiantes.             ");
+                    Console.WriteLine();
+                    Console.Write("Digite el código:                  ");
+                    lists[i].studentcode = int.Parse(Console.ReadLine());
+                    Console.Write("Digite el nombre del estudiante:    ");
+                    lists[i].studentname = Console.ReadLine();
+                    Console.Write("Ingrese el sexo:     ");
+                    lists[i].studentsex = Console.ReadLine();
+                    if (lists[i].studentsex == fem)
+                    {
+                        cont1++;
+
+                    }
+                    if (lists[i].studentsex == mas)
+                    {
+                        cont2++;
+                    }
+                    Console.Write("Ingrese la edad:");
+                    lists[i].age = int.Parse(Console.ReadLine());
+                    if (lists[i].age <= 18)
+                    {
+                        minors++;
+
+                    }
+                    if (lists[i].age >= 19 && lists[i].age <= 29)
+                    {
+                        young++;
+                    }
+                    if (lists[i].age >= 30)
+                    {
+                        adults++;
+
+                    }
+
+
+                    Console.WriteLine("Desea agregar otro estudiante 0-Sí 1-No");
+                    r = int.Parse(Console.ReadLine());
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Registro de estudiantes lleno...");
+                    r = 1;
+                    Console.ReadKey();
+                }
+            }
+
+
+        }
+        static void ShowStudents()
+        {
+            int reg = 0;
+            Console.Clear();
+
+
+            Console.WriteLine("======================================================================================\n" +
+
+                                                "                     " + "Listado Estudiantil\n" +
+                           "=====================================================================================");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Universidad UH");
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Reg          Código         Nombre          Sexo           Edad    ");
+            for (i = 0; i < lists.Length; i++)
+            {
+                reg = i + 1;
+                Console.WriteLine(reg + "             " + lists[i].studentcode + "          " + lists[i].studentname + "             " + lists[i].studentsex + "              " + lists[i].age);
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("=====================================================================================");
+            Console.WriteLine("Fin del informe");
+            Console.WriteLine();
+            Console.WriteLine("Presione la tecla INTRO para volver al menú");
+
+            Console.ReadKey();
+        }
+
+
+        static void SeekStudents()
+        {
+
+            int Code = 0;
+            int reg = 0;
+            int r = 0;
+            bool found = false;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Universidad UH");
+                Console.WriteLine("Búsquedad de Estudiantes           ");
+                Console.WriteLine("Digite el código del estudiante a buscar");
+                Code = int.Parse(Console.ReadLine());
+                for (i = 0; i < lists.Length; i++)
+                {
+                    if (Code == lists[i].studentcode)
+                    {
+                        reg = i + 1;
+                        Console.WriteLine("Registro: " + reg);
+                        Console.WriteLine("Código:   " + lists[i].studentcode);
+                        Console.WriteLine("Nombre:   " + lists[i].studentname);
+                        Console.WriteLine("Sexo:     " + lists[i].studentsex);
+                        Console.WriteLine("Edad:     " + lists[i].age);
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("No se encontro ese código");
+                    found = false;
+                }
+                Console.WriteLine("Desea buscar otro estudiante 0-Sí 1-No");
+                r = int.Parse(Console.ReadLine());
+            }
+        }
+        static void modifydata()
+        {
+            int option = 0;
+            int user = 0;
+            int r = 0;
+
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Universidad Hispanoamericana");
+                Console.WriteLine("Modificación de Información");
+                Console.WriteLine("Digite el código del estudiante a buscar");
+                user = int.Parse(Console.ReadLine());
+                for (i = 0; i < lists.Length; i++)
+                {
+                    if (user == lists[i].studentcode)
+                    {
+
+                        while (option != 5)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ingrese la opcion que desea cambiar");
+                            Console.WriteLine("1.Nombre");
+                            Console.WriteLine("2.Sexo");
+                            Console.WriteLine("3.Edad");
+                            Console.WriteLine("4.Ver cambios");
+                            Console.WriteLine("5.Salir");
+                            option = int.Parse(Console.ReadLine());
+                            switch (option)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    Console.WriteLine("Digite el nuevo nombre");
+                                    lists[i].studentname = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.Clear();
+                                    Console.WriteLine("Digite el sexo");
+                                    lists[i].studentsex = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.Clear();
+                                    Console.Write("Ingrese la edad:");
+                                    lists[i].age = int.Parse(Console.ReadLine());
+
+                                    break;
+                                case 4:
+
+                                    Console.Clear();
+                                    Console.WriteLine("Actuales Modificaciones");
+                                    if (user == lists[i].studentcode)
+                                    {
+
+                                        Console.WriteLine("Nombre:   " + lists[i].studentname);
+                                        Console.WriteLine("Sexo:     " + lists[i].studentsex);
+                                        Console.WriteLine("Edad:   " + lists[i].age);
+
+                                    }
+
+                                    Console.WriteLine("Desea realizar otro cambio 0-Sí 1-No");
+                                    r = int.Parse(Console.ReadLine());
+
+                                    break;
+                                case 5:
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (user != lists[i].studentcode)
+                    {
+                        Console.WriteLine("No se encontro ese código");
+
+
+                    }
+                }
+            }
+        }
+        static void deletestudents()
+        {
+            int codigo = 0;
+            int reg = 0;
+            int r = 0;
+            bool found = false;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Registro Universidad Hispanoamericana");
+                Console.WriteLine("Digite el código del estudiante que desa borrar");
+                codigo = int.Parse(Console.ReadLine());
+                for (i = 0; i < lists.Length; i++)
+                {
+                    if (codigo == lists[i].studentcode)
+                    {
+                        Console.Clear();
+
+                        reg = i + 1;
+                        reg = 0;
+                        lists[i].studentcode = 0;
+                        lists[i].studentname = "";
+                        lists[i].studentsex = "";
+                        lists[i].age = 0;
+
+
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("No se encontro ese ID");
+                    found = false;
+                }
+                Console.WriteLine("Desea borrar otro estudiante 0-Sí 1-No");
+                r = int.Parse(Console.ReadLine());
+            }
+        }
+
+
+        static void Main()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite la cantidad de estudiantes a ingresar");
+                size2 = int.Parse(Console.ReadLine());
+
+                if (size2 < size)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Digite minimo 5 estudiantes");
+                    Console.ReadKey();
+
+
+                }
+
+                else
+                {
+
+
+                    lists = new list[size2];
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Menú Principal");
+                        Console.WriteLine("1. Ingreso de Estudiantes.");
+                        Console.WriteLine("2. Modificación de Estudiantes.");
+                        Console.WriteLine("3. Borrado de Estudiantes.");
+                        Console.WriteLine("4. Búsquedad de Estudiantes.");
+                        Console.WriteLine("5. Listado de Estudiantes.");
+                        Console.WriteLine("6. Informe clasificación de estudiantes.");
+                        Console.WriteLine("7. Salir.");
+                        Console.WriteLine("Selecciones una opción");
+                        option = int.Parse(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                {
+                                    AddStudents();
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    modifydata();
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    deletestudents();
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    SeekStudents();
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    ShowStudents();
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    count();
+                                    break;
+                                }
+                            case 7:
+
+                                break;
+                            default:
+                                {
+                                    Console.WriteLine("Seleccione inválida..");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+                        }
+
+                    }  while (size2 >= size && option !=7);
+                }
+            }while (option != 7);
+        }
+    }
+}
+
